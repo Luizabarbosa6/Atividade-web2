d3760edb3c8f307482c45a093f17c07566583b81
Luizabarbosa6 <153461981+Luizabarbosa6@users.noreply.github.com>
2025-03-10 17:13:15 -0300

    :fire:

--- index.js
+++ index.js
@@ -1,4 +1,2 @@
-document.getElementById("irparaproximapag")?.addEventListener("click", function() {
-    window.location.href = "pag2.html"; 
-});
+
 
---
+++ serviceWoker.js
@@ -0,0 +1,31 @@
+const staticAnimeSite = 'my-favorite-anime-v1';
+const assets = [
+  "/",
+  "/index.html",
+  "/css/styles.css",
+  "/img/icone.png",
+  "/img/icone2.png",
+  "/img/icone3.png",
+  "/img/icone4.png",
+  "/img/icon5.png",
+  "/img/icone1.png",
+  "/img/72.png",
+  "/img/appstore.png",
+  "/img/playstore.png"
+];
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticAnimeSite).then(cache => {
+      cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request);
+    })
+  );
+});
