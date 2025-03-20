f624dc4c0c59a8b2e94adfc0fa36a95f8220da1c
Luizabarbosa6 <153461981+Luizabarbosa6@users.noreply.github.com>
2025-03-19 21:10:59 -0300

    :memo:

--- serviceWoker.js
+++ serviceWoker.js
@@ -3,14 +3,16 @@ const assets = [
   "/",
   "/index.html",
   "/css/styles.css",
-  "/img/android-icon-48x48.png",
-  "/img/android-icon-72x72.png",
-  "/img/android-icon-96x96.png",
-  "/img/android-icon-144x144.png",
-  "/img/android-icon-152x152.png",
-  "/img/android-icon-180x180.png",
+  "/img/icon/android-icon-48x48.png",
+  "/img/icon/android-icon-72x72.png",
+  "/img/icon/android-icon-96x96.png",
+  "/img/icon/android-icon-144x144.png",
+  "/img/icon/android-icon-152x152.png",
+  "/img/icon/android-icon-180x180.png",
   "img/icon/favicon-16x16.png",
-  "img/icon/favicon-32x32.png"
+  "img/icon/favicon-32x32.png",
+  "img/icon/icon-512x512.png",
+  "img/icon/ms-icon-310x310.png"
 
 ];
 
